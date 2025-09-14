3217d95425a259d9e6c7624b7d80052f9fd78761
peep123123 <dovmf5258@gmail.com>
2025-09-15 02:06:42 +0900

    feat: Improve UI/UX

--- not to do list frontend/src/components/NotTodoList.vue
+++ not to do list frontend/src/components/NotTodoList.vue
@@ -17,7 +17,7 @@
         <!-- v-if: Vue의 조건부 렌더링 지시어, 특정 조건이 true일 때만 해당 요소 화면 표시
              v-else: v-if가 false 일때 실행 --> 
         <div v-if="notTodos.length === 0" class="loading-message">
-            <p>Loading...</p>
+            <p>고치고 싶은 습관을 추가해 보세요.</p>
         </div>
         <ul v-else>
             <!-- v-for: Vue의 반복문 
@@ -40,12 +40,12 @@
     // ref: 일반 변수를 반응형(Reactive)으로 만듬
     // onMounted: 컴포넌트가 화면에 처음 나타났을때 특정 코드를 실행하도록 하는 함수
     // axios: 백엔드와 HTTP통신을 하기위한 라이브러리
-    import { ref, onMounted } from 'vue';
+    import { ref, onMounted, nextTick } from 'vue';
     import axios from 'axios'
 
     const notTodos = ref([]);       // notTodos 라는 반응형 변수 생성
 
-    //데이터를 불러오는 함수를 별로로 분리
+    // 데이터를 불러오는 함수를 별로로 분리
     const fetchTodos = async () => {
         try {
             // 1. GET 요청으로 백엔드에서 데이터 가져오기
@@ -59,41 +59,48 @@
         }
     };
 
-    const newNotTodoTitle = ref('');        //새로운 하지 말아야 할 일 제목을 저장할 반응형 변수
+    const newNotTodoTitle = ref('');        // 새로운 안 할 일 제목을 저장할 반응형 변수
 
-    //새로운 항목을 추가하는 함수
+    // 새로운 항목을 추가 함수
     const addNotTodo = async () => {
         // 입력창이 비어있으면 함수를 종료
         if (newNotTodoTitle.value.trim() === '') {
-            alert('하지 말아야 할 일을 입력하세요.');
+            alert('안 할 일을 입력하세요.');
             return;
         }
         try {
-            //1. POST 요청으로 백엔드에 데이터 전송
+            // 1. POST 요청으로 백엔드에 데이터 전송
             await axios.post('http://localhost:8080/nottodo', {
                 title: newNotTodoTitle.value,
                 completed: false
             });
-            //2. 입력창 비우기
+            
+            // 2. 입력창 비우기
             newNotTodoTitle.value = '';
-            //3.새로운 목록을 다시 불러와 화면 업데이트
+
+            // 3. nextTick을 사용하여 DOM이 업데이트된 후 커서 위치시키기
+            await nextTick(() => {
+                document.querySelector('.input-container input').focus();
+            });
+            
+            // 4.새로운 목록을 다시 불러와 화면 업데이트
             await fetchTodos();
         } catch (error) {
             console.error("Error fetching notTodos: ", error);
         }
     }
 
-    // 하지 말아야 될 일 완료 기능 추가 함수
+    // 안 할 일 완료 기능 함수
     const toggleComplete = async (todo) => {
         try {
-            //1. 백엔드에 PUT 요청 보내기(completed 상태 토글)
+            // 1. 백엔드에 PUT 요청 보내기(completed 상태 토글)
             await axios.put(`http://localhost:8080/nottodo/${todo.id}`, {
-                //...todo spread 문법: 기존 todo 객체의 모든 속송을 그대로 유지하고, 
+                // ...todo spread 문법: 기존 todo 객체의 모든 속송을 그대로 유지하고, 
                 // compelted 속성만 !todo.completed로 바꿔서 백엔드에 보냄
                 ...todo,
                 completed: !todo.completed
             });
-            //2. 전체 목록을 다시 불러와 화면 업데이트
+            // 2. 전체 목록을 다시 불러와 화면 업데이트
             await fetchTodos();
         } catch (error) {
             console.error("Error updating todo:", error);
@@ -103,9 +110,9 @@
     // 항목 삭제 함수
     const deleteNotTodo = async (id) => {
         try {
-            //1. qordpsemdp DELETE 요청 보내기
+            // 1. qordpsemdp DELETE 요청 보내기
             await axios.delete(`http://localhost:8080/nottodo/${id}`);
-            //2. 전체 목록을 다시 불러와 화면 업데이트
+            // 2. 전체 목록을 다시 불러와 화면 업데이트
             await fetchTodos();
         } catch (error) {
             console.error("Error deleting todo:", error);
